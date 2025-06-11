814876f31639d819284b17845b111296e0737ba4
yuwnloy <yuwnloyblog@163.com>
2025-06-11 14:56:29 +0800

    add sql file

---
+++ docs/bot.sql
@@ -0,0 +1,25 @@
+CREATE TABLE IF NOT EXISTS `appinfos` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `app_name` varchar(50) DEFAULT NULL,
+  `app_key` varchar(50) DEFAULT NULL,
+  `app_secret` varchar(50) DEFAULT NULL,
+  `api_secure_key` varchar(50) DEFAULT NULL,
+  `api_url` varchar(200) DEFAULT NULL,
+  `app_status` tinyint DEFAULT NULL,
+  `created_time` datetime(3) DEFAULT CURRENT_TIMESTAMP(3),
+  `updated_time` datetime(3) DEFAULT CURRENT_TIMESTAMP(3) ON UPDATE CURRENT_TIMESTAMP(3),
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `uniq_appkey` (`app_key`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE IF NOT EXISTS `telebotrels` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `tele_bot_id` varchar(50) NOT NULL,
+  `user_id` varchar(50) NOT NULL,
+  `bot_token` varchar(100) DEFAULT NULL,
+  `created_time` datetime(3) DEFAULT CURRENT_TIMESTAMP(3),
+  `app_key` varchar(20) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `uniq_botid` (`app_key`,`tele_bot_id`),
+  KEY `idx_userid` (`app_key`,`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
---
+++ routers/router.go
@@ -0,0 +1,7 @@
+package routers
+
+import "github.com/gin-gonic/gin"
+
+func Route(eng *gin.Engine, prefix string) {
+	eng.Use()
+}
