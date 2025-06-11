c16bd862e2313dae00adca67eacadb34b79f8304
yuwnloy <yuwnloyblog@163.com>
2025-06-11 17:17:25 +0800

    add test

--- testmain/main.go
+++ testmain/main.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+import (
+	"bot-connector/configures"
+	"bot-connector/services"
+	"fmt"
+)
 
+func main() {
+	if err := configures.InitConfigures(); err != nil {
+		fmt.Println("Init Configures failed.", err)
+		return
+	}
+	apiKey, err := services.GenerateApiKey("appkey", "", "")
+	fmt.Println(apiKey, err)
 }
